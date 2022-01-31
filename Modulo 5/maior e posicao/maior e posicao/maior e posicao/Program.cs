15d702565ae79b3c92ac059db1414c35a7a76c72
Ecxpectro <henriquescunha12345@gmail.com>
2022-01-31 19:12:41 -0300

    finalizado

---
+++ Modulo 5/maior e posicao/maior e posicao/maior e posicao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace primeiro
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int maior, posicao;
+            maior = int.Parse(Console.ReadLine());
+            posicao = 1;
+            
+            for (int i = 2; i <= 100; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+             
+                if (n > maior)
+                {
+                    maior = n;
+                    posicao = i;
+                }
+
+
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+
+
+        }
+    }
+}
