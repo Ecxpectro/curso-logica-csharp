37fa9212fc305c58803e8a45e2542b531402faa7
Ecxpectro <henriquescunha12345@gmail.com>
2022-02-01 19:40:45 -0300

    finalizado

---
+++ Modulo 5/modulo 5/maior e posicao/beecrowd 1153/beecrowd 1153/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fat = 0;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (i == 1)
+                {
+                    fat = n * (n - i);
+                }
+                else
+                {
+                    fat =fat * (n - i);                                     
+                }
+
+            }
+            
+            Console.WriteLine(fat);
+
+        }
+    }
+}
