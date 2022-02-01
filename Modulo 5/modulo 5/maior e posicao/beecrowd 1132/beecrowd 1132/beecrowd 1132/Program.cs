ad9756b10f6a04f8d129da9b8a91fd9b380ab0a6
Ecxpectro <henriquescunha12345@gmail.com>
2022-01-31 21:37:23 -0300

    finalizado

---
+++ Modulo 5/modulo 5/maior e posicao/beecrowd 1132/beecrowd 1132/beecrowd 1132/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, y, soma = 0;
+            x = int.Parse(Console.ReadLine());
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = x; i <= n; i++)
+            {
+                if ( i % 13 != 0)
+                {
+                    soma += i;
+                }
+
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
