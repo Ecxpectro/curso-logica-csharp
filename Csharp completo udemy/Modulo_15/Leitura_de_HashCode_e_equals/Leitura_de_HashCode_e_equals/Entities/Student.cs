ff1005dd02cc4b071992a910d133fec70bc23637
Ecxpectro <henriquescunha12345@gmail.com>
2022-04-25 15:41:56 -0300

    aplicação de dictionary e HashCode finalizado

---
+++ Csharp completo udemy/Modulo_15/Leitura_de_HashCode_e_equals/Leitura_de_HashCode_e_equals/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace Leitura_de_HashCode_e_equals.Entities
+{
+    class Student
+    {
+        public int StudentCode { get; set; }
+
+        public Student(int studentCode)
+        {
+            StudentCode = studentCode;
+        }
+
+        public override int GetHashCode()
+        {
+            return StudentCode.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+            return StudentCode.Equals(other.StudentCode);
+        }
+    }
+}
---
+++ Csharp completo udemy/Modulo_15/Leitura_de_HashCode_e_equals/Leitura_de_HashCode_e_equals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Leitura_de_HashCode_e_equals.Entities;
+using System.Collections.Generic;
+namespace Leitura_de_HashCode_e_equals
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> set = new HashSet<Student>();
+
+            Console.Write("How many students for course A?");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                set.Add(new Student(code));
+            }
+            Console.Write("How many students for course B?");
+            int b = int.Parse(Console.ReadLine());
+            for (int i = 0; i < b; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                set.Add(new Student(code));
+            }
+            Console.Write("How many students for course C?");
+            int c = int.Parse(Console.ReadLine());
+            for (int i = 0; i < c; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                set.Add(new Student(code));
+            }
+            Console.WriteLine("Total student: " + set.Count);
+        }
+    }
+}
