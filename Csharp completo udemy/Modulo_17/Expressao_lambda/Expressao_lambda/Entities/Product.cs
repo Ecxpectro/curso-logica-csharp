5175596dbac5086e52e34ed9a2f4750f6f072c43
Ecxpectro <henriquescunha12345@gmail.com>
2022-05-10 15:54:29 -0300

    Exercício de lambda terminado

---
+++ Csharp completo udemy/Modulo_17/Expressao_lambda/Expressao_lambda/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Expressao_lambda.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
